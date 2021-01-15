6f420b3eb0e004f4bf1a5c716a18aaf035307e8e
Marianne <Marianne@DESKTOP-R0MM6V2.mshome.net>
2021-01-15 16:59:31 +0100

    inscritpion serlvet

--- src/fr/eni/projetEncheres/Servlets/Inscription.java
+++ src/fr/eni/projetEncheres/Servlets/Inscription.java
@@ -6,6 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+
+import fr.eni.projetEncheres.bll.UtilisateursBll;
+import fr.eni.projetEncheres.bo.UtilisateursBo;
+import fr.eni.projetEncheres.dal.UtilisateursDal;
 
 /**
  * Servlet implementation class Inscription
@@ -14,9 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Inscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Inscription() {
         super();
         // TODO Auto-generated constructor stub
@@ -26,7 +29,6 @@ public class Inscription extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/InscriptionJSP.jsp").forward(request, response);
 	}
 
@@ -34,8 +36,41 @@ public class Inscription extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String codePostal = request.getParameter("codePostal");
+		String ville = request.getParameter("ville");
+		String motDePasse = request.getParameter("motDePasse");
+		
+		UtilisateursBo premier = new UtilisateursBo();
+		 premier.setPseudo(pseudo);
+		 premier.setNom(nom);
+		 premier.setPrenom(prenom);
+		 premier.setEmail(email);
+		 premier.setTelephone(telephone);
+		 premier.setRue(rue);
+		 premier.setCodePostal(codePostal);
+		 premier.setVille(ville);
+		 premier.setMotDePasse(motDePasse);
+		 
+		/* request.setAttribute("pseudo", pseudo);
+		 request.setAttribute("nom", nom);
+		 request.getRequestDispatcher("/WEB-INF/JSP/Test.jsp").forward(request, response); */
+		
+		 
+		 
+		
+			UtilisateursDal.insert(premier);
+			request.getRequestDispatcher("/WEB-INF/JSP/Gagne.jsp").forward(request, response);
+		}
+	
+		 
 	}
+	
+	
 
-}
+
